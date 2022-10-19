2d8f919a4ab2dfb1f9e3a5feb4220c5e1479fc87
Ashley Pearson <ashleypearson@Ashleys-MacBook-Air.local>
2022-10-18 20:17:44 -0700

    second commit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,22 @@
 $(function () {});
-
+//Display current date
 var m = moment()
 $("#currentDay").text(moment().format("MMM DD, YYYY"));
 
 var now = moment().format("H A");
+
+
+
+$(".saveBtn").click(function() {
+    eventText = $(this).siblings(".input").val();
+    console.log(eventText);
+    eventTime = $(this).siblings(".hour").text();
+    console.log(eventTime);
+    localStorage.setItem(eventTime, JSON.stringify(eventText));
+    localStorage.getItem(eventTime, JSON.stringify(eventText));
+
+    
+});
+
+
+
